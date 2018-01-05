1c7e92e7d560e3aae56a4620796e8bd0f348656d
Jimmey-Jiang <2181130@qq.com>
2018-01-05 10:18:04 +0800

    反序列化JSON到给定的匿名对象

--- Utility基础类大全/JSON操作/ConvertJson.cs
+++ Utility基础类大全/JSON操作/ConvertJson.cs
@@ -6,6 +6,8 @@ using System.Data;
 using System.Reflection;
 using System.Collections;
 using System.Data.Common;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace Common.Utility
 {
@@ -318,6 +320,101 @@ namespace Common.Utility
             jsonString.Append("]");
             return jsonString.ToString();
         }
+        #endregion
+
+
+        #region Datatable转换为Json 2
+
+        /// <summary>
+        /// Datatable转换为Json 2
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static List<Dictionary<string, object>> DataTableToDictionary(DataTable dt)
+        {
+            List<Dictionary<string, object>> list = new List<Dictionary<string, object>>();
+            foreach (DataRow dr in dt.Rows)
+            {
+                Dictionary<string, object> result = new Dictionary<string, object>();
+                foreach (DataColumn dc in dt.Columns)
+                {
+                    result.Add(dc.ColumnName, dr[dc].ToString());
+                }
+                list.Add(result);
+            }
+            return list;
+        }
+        #endregion
+
+
+        #region SerializeObject
+
+        /// <summary>
+        /// SerializeObject
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public static string SerializeObject(object o)
+        {
+            string json = JsonConvert.SerializeObject(o);
+            return json;
+        }
+        #endregion
+
+
+        #region 解析JSON字符串生成对象实体
+
+        /// <summary>
+        /// 解析JSON字符串生成对象实体
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="json">json字符串(eg.{"ID":"112","Name":"石子儿"})</param>
+        /// <returns>对象实体</returns>
+        public static T DeserializeJsonToObject<T>(string json) where T : class
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            StringReader sr = new StringReader(json);
+            object o = serializer.Deserialize(new JsonTextReader(sr), typeof(T));
+            T t = o as T;
+            return t;
+        }
+
+        #endregion
+
+        #region 解析JSON数组生成对象实体集合
+
+        /// <summary>
+        /// 解析JSON数组生成对象实体集合
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="json">json数组字符串(eg.[{"ID":"112","Name":"石子儿"}])</param>
+        /// <returns>对象实体集合</returns>
+        public static List<T> DeserializeJsonToList<T>(string json) where T : class
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            StringReader sr = new StringReader(json);
+            object o = serializer.Deserialize(new JsonTextReader(sr), typeof(List<T>));
+            List<T> list = o as List<T>;
+            return list;
+        }
+
+        #endregion
+
+        #region 反序列化JSON到给定的匿名对象
+
+        /// <summary>
+        /// 反序列化JSON到给定的匿名对象.
+        /// </summary>
+        /// <typeparam name="T">匿名对象类型</typeparam>
+        /// <param name="json">json字符串</param>
+        /// <param name="anonymousTypeObject">匿名对象</param>
+        /// <returns>匿名对象</returns>
+        public static T DeserializeAnonymousType<T>(string json, T anonymousTypeObject)
+        {
+            T t = JsonConvert.DeserializeAnonymousType(json, anonymousTypeObject);
+            return t;
+        }
+
         #endregion
     }
 }
